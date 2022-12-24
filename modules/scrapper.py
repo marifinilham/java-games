677b99bc83c97c221400c00f983a0eafcccdb6c0
marifinilham <arifinmblygf@gmail.com>
2022-12-24 21:04:09 +0700

    add class Scrap

--- modules/scrapper.py
+++ modules/scrapper.py
@@ -1,12 +1,47 @@
 from requests import Session, get, post
 from bs4 import BeautifulSoup as be
 
-class Dedomil:
+version = '1.0.4'
+
+class Scrap:
+	def __init__(self):
+		self.cli()
+
+	def cli(self):
+		ps1 = f'[java-games-{version}]'
+		if idgame := self.config['id']:
+			ps1 += f'[{idgame}]'
+		ps1 += ' '
+
+		prompt = input(ps1).split()
+		if len(prompt) == 0:
+			return self.cli()
+
+		key = prompt[0]
+		param = prompt[1:]
+
+		if key == 'search':
+			self.search(param)
+
+		elif key == 'set':
+			self.setting(param)
+
+		elif key == 'exit':
+			return
+
+		self.cli()
+
+class Dedomil(Scrap):
 	def __init__(self):
 		self.sesi = Session()
 		self.host = 'http://dedomil.net'
+		self.config = {
+			'id': 0
+		}
+		super().__init__()
 
-	def search(self, q):
+	def search(self, args):
+		q = ' '.join(args)
 		path = self.host+'/games/search'
 		s = self.sesi.post(
 			path,
@@ -23,3 +58,13 @@ class Dedomil:
 		games = web.select('div[class=GMENU]')
 		for game in games:
 			print(game.a['href'], game.a.text)
+
+	def setting(self, args):
+		key, val = args
+		if key not in self.config.keys():
+			return print('key not found:', key)
+
+		if type(self.config[key]) == int and not val.isdigit():
+			return print('value type not match')
+
+		self.config[key] = val
