ede08dcf8aab34bec6b7196e76fc6d01d5caa59e
marifinilham <arifinmblygf@gmail.com>
2022-12-25 17:10:15 +0700

    phead

--- modules/func.py
+++ modules/func.py
@@ -1,3 +1,8 @@
+# clean screen
 clean = lambda: print("\033c", end="")
 
+# spacer
 space = lambda x: "   "+"\b"*(len(x)-1)
+
+# idk
+phead = lambda x,y: print(f'\u001b[4m{x}  {space(x)}{y}\033[0m')
